fb130b16379efc8e9fe417246409be74effa6c9d
DarkDev <darkdev_brand@riseup.net>
2024-04-19 13:52:54 +0300

    refactoring theme switcher

--- src/lib/shared/utils/svelteThemes/constantMEDIA.ts
+++
@@ -1,4 +0,0 @@
-// copied from https://github.com/beynar/svelte-themes/blob/main/src/lib/constants.ts
-const MEDIA = '(prefers-color-scheme: dark)';
-
-export default MEDIA;
--- src/lib/shared/utils/svelteThemes/setTheme.ts
+++
@@ -1,5 +0,0 @@
-import { themeStore } from '$shared';
-
-const setTheme = (theme: string): void => themeStore.update((store) => ({ ...store, theme }));
-
-export default setTheme;
