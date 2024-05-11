cefb929f8b798a7952c1e769c1b5d10761153c9c
DarkDev <darkdev_brand@riseup.net>
2024-05-11 23:43:13 +0300

    upd api

--- src/lib/widgets/index.js
+++ src/lib/widgets/index.js
@@ -1,7 +1,19 @@
-// import from './ui/';
+import Hero from './landing/Hero.svelte';
+import Footer from './landing/Footer.svelte';
+import About from './landing/About.svelte';
+import Goals from './landing/Goals.svelte';
+import Header from './landing/Header.svelte';
+import Reviews from './landing/Reviews.svelte';
+import Contact from './landing/Contact.svelte';
 
 
 
-// export {
-
-// };
+export {
+  Header,
+  Contact,
+  Hero,
+  Footer,
+  Goals,
+  About,
+  Reviews
+};
